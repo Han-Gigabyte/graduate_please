d6b852061391c18b8c8806ed31cb588645871506
Han <illuminous0814@naver.com>
2024-11-21 12:17:24 +0900

    Added Changes v0.0.5
    Added PlayerPrefs for saving level changes & load character level informations
    Added levelup mechanisms
    Fixed scene movements

--- Assets/Codes/CharacterData.cs
+++ Assets/Codes/CharacterData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CharacterData : ScriptableObject
 {
     public string characterName;  // 캐릭터 이름
-    public int level;             // 레벨
+    public int level = 1;         // 레벨 (초기값을 1로 설정)
     public string description;    // 설명
     public int vitality;          // 생명력
     public int power;             // 파워
